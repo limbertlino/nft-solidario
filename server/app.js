e0149a66a588c9a68946d26d7b673763b78b6df9
L. Lino Mattos <limbertlino@gmail.com>
2022-11-30 07:50:39 -0400

    configuracion basica del server

---
+++ server/app.js
@@ -0,0 +1,13 @@
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const { router } = require("./routes")
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// app.use(morgan("tiny"))
+app.use(morgan("tiny"))
+
+app.use("/api/v1/", router)
+
+app.listen(PORT, () => console.log(`Servidor levantado en puerto ${PORT}`))
